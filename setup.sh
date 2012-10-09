acc68baeaa558cfd16cec9b80b1c471f77c0e442
thomp <met600@gmail.com>
2012-10-08 23:29:39 -0700

    added colorama and pyyaml tar balls to lib directory, and fixed setup script to install from both

--- setup.sh
+++ setup.sh
@@ -1,2 +1,26 @@
-sudo easy_install colorama
-sudo easy_install pyyaml
+
+
+
+## Install colorama
+cd lib
+tar xzvf colorama-0.2.4.tar.gz
+cd colorama-0.2.4
+sudo python setup.py install
+cd ..
+sudo rm -rf colorama-0.2.4
+cd ..
+
+
+## Install PyYAML
+cd lib
+tar xzvf PyYAML-3.10.tar.gz
+cd PyYAML-3.10
+sudo python setup.py install
+cd ..
+sudo rm -rf PyYAML-3.10
+cd ..
+
+
+exit 0
+#sudo easy_install colorama
+#sudo easy_install pyyaml
