4244b021c0eebceefdd7e8f82e0108f9f7e56a6b
jj <jj@jj-VirtualBox.(none)>
2014-09-20 13:01:09 -0700

    small change to init

--- AsciiMap.py
+++ AsciiMap.py
@@ -5,9 +5,15 @@ class AsciiMap(object):
     def __init__(self):
         self.arr = []
         
-    def append_row(self,tile):
-        self.arr.append(tile)
-
-    def get_tile(self,num):
+    def append_row(self,row):
+        self.arr.append(row)
+        
+    def get_row(self,num):
         if num < len(self.arr):
             return self.arr[num]
+        else: print "invalid index"
+
+    def get_rowlen(self,num):
+        if num < len(self.arr):
+            return len(self.arr[num])
+        else: print "invalid index"
